83b1320062376c6f5b52f7f1cc69b715cf54a12a
ilhamsj <ilhamsaputrajati@gmail.com>
2018-05-26 22:47:54 +0700

    SQL Terbaru

--- assets/penjualan_mobil.sql
+++ assets/penjualan_mobil.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2018 at 09:15 AM
+-- Generation Time: May 26, 2018 at 05:45 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -25,166 +25,212 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cars`
+-- Table structure for table `gallery`
 --
 
-CREATE TABLE `cars` (
-  `id_car` int(11) NOT NULL,
-  `car_nopolice` varchar(10) NOT NULL,
-  `car_merk` varchar(20) NOT NULL,
-  `car_model` varchar(20) NOT NULL,
-  `car_color` varchar(10) NOT NULL,
-  `car_years` int(4) NOT NULL,
-  `car_purchase` int(10) NOT NULL,
-  `car_price` int(6) NOT NULL,
-  `date_purchase` timestamp NULL DEFAULT NULL,
-  `date_sold` timestamp NULL DEFAULT NULL,
-  `car_image` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `cars`
---
-
-INSERT INTO `cars` (`id_car`, `car_nopolice`, `car_merk`, `car_model`, `car_color`, `car_years`, `car_purchase`, `car_price`, `date_purchase`, `date_sold`, `car_image`) VALUES
-(31, 'AB6316CS', 'BMW', 'Audi', 'Hitam', 2010, 400000000, 500000000, '2018-05-08 17:00:00', '2018-05-30 17:00:00', 'CR-Inline-top-picks-Toyota-Yaris-02-17.jpg'),
-(48, 'AB6316YZ', 'Honda', 'Brio', 'Blue', 2015, 200000000, 400000000, '2018-05-22 17:00:00', '0000-00-00 00:00:00', 'lamborghini-urus.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `confirm`
---
-
-CREATE TABLE `confirm` (
-  `id_confirm` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `text` varchar(500) NOT NULL
+CREATE TABLE `gallery` (
+  `id_gallery` int(11) NOT NULL,
+  `id_mobil` varchar(11) NOT NULL,
+  `gambar` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transactions`
+-- Table structure for table `member`
 --
 
-CREATE TABLE `transactions` (
-  `id_transaction` int(11) NOT NULL,
-  `id_car` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `transaction_date` timestamp NULL DEFAULT NULL,
-  `transaction_status` int(11) NOT NULL,
-  `transaction_count` int(10) DEFAULT NULL,
-  `transaction_bukti` varchar(50) NOT NULL,
-  `description` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `transactions`
---
-
-INSERT INTO `transactions` (`id_transaction`, `id_car`, `username`, `transaction_date`, `transaction_status`, `transaction_count`, `transaction_bukti`, `description`) VALUES
-(10, 4, 'ilham', '2018-05-06 17:00:00', 0, 300000000, '', 'Toyota Mustang Hijau 2010'),
-(11, 4, 'arti', '2018-05-06 17:00:00', 2, 300000000, 'jodis', 'Toyota Mustang Hijau 2010'),
-(12, 2, 'arti', '2018-05-06 17:00:00', 1, 200000000, '206881.png', 'Toyota T8000 Biru 2015'),
-(13, 3, 'arti', '2018-05-07 02:49:42', 1, 200000000, '20106808_1967474220156906_8000425653938385383_n.pn', 'Honda Brio Hitam 2016'),
-(14, 1, 'arti', '2018-05-07 02:51:28', 1, 400000000, '', 'Honda Brio Merah 2017'),
-(15, 4, 'arti', '2018-05-07 03:02:34', 1, 300000000, '53efb9f1-3a2a-4238-ab25-88827aedf93e.jpg', 'Toyota Mustang Hijau 2010'),
-(16, 4, 'arti', '2018-05-07 03:06:43', 2, 300000000, '20106808_1967474220156906_8000425653938385383_n.pn', 'Toyota Mustang Hijau 2010'),
-(17, 4, 'ilham', '2018-05-07 03:19:31', 2, 300000000, '', 'Toyota Mustang Hijau 2010'),
-(18, 4, 'arti', '2018-05-07 04:37:37', 0, 300000000, '', 'Toyota Mustang Hijau 2010'),
-(19, 4, 'arti', '2018-05-07 04:49:14', 0, 300000000, '', 'Toyota Mustang Hijau 2010'),
-(20, 2, 'jodis', '2018-05-07 05:00:04', 1, 200000000, 'ad2.jpg', 'Toyota T8000 Biru 2015'),
-(21, 4, 'arti', '2018-05-07 05:15:55', 1, 300000000, '18872.jpg', 'Toyota Mustang Hijau 2010'),
-(22, 1, 'jodis', '2018-05-07 06:51:18', 0, 400000000, '', 'Honda Brio Merah 2017'),
-(23, 1, 'arti', '2018-05-07 09:29:06', 0, 400000000, '', 'Honda Brio Merah 2017'),
-(24, 7, 'jodis', '2018-05-08 05:28:54', 1, 500000000, '53efb9f1-3a2a-4238-ab25-88827aedf93e.jpg', 'BMW Audi Hijau Kuni 2016'),
-(25, 1, 'jodis', '2018-05-08 05:36:02', 0, 450000000, '', 'Honda Jazza Putih 2015'),
-(26, 1, 'jodis', '2018-05-08 05:36:08', 0, 450000000, '', 'Honda Jazza Putih 2015'),
-(27, 7, 'arti', '2018-05-08 05:37:35', 0, 500000000, '', 'BMW Audi Hijau Kuni 2016'),
-(28, 1, 'arti', '2018-05-08 05:42:46', 0, 450000000, '', 'Honda Jazza Putih 2015'),
-(29, 48, 'jodis', '2018-05-09 01:12:17', 2, 400000000, 'maxresdefault.jpg', 'Honda Brio Blue 2015');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id_user` int(11) NOT NULL,
+CREATE TABLE `member` (
+  `id_member` varchar(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `level` int(11) NOT NULL,
-  `fullname` varchar(30) NOT NULL,
-  `nik` varchar(20) NOT NULL,
-  `adress` varchar(200) NOT NULL
+  `level` enum('0','1') NOT NULL,
+  `nm_lgkp` varchar(30) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `alamat` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `member`
 --
 
-INSERT INTO `users` (`id_user`, `username`, `password`, `level`, `fullname`, `nik`, `adress`) VALUES
-(1, 'jodis', '123', 0, 'Ilham Saputrajati', '', ''),
-(2, 'admin', '123', 1, '', '', ''),
-(3, 'arti', '123', 0, '', '', '');
+INSERT INTO `member` (`id_member`, `username`, `password`, `level`, `nm_lgkp`, `telp`, `email`, `alamat`) VALUES
+('USR0009', 'admin', 'f6fdffe48c908deb0f4c3bd36c032e72', '1', 'Admin S', '089666445551', 'admin@mobilindo.example.co', 'Gebang, Girisuko, Panggang, Gunungkidul, Girisuko, Kec. Panggang, Kab. Gunung Kidul Prov. D.I. Yogyakarta'),
+('USR0010', 'arti', '202cb962ac59075b964b07152d234b70', '0', 'Arti Khansa', '089666445551', 'artikhansa@example.co', 'Street addresses: 445 Mount Eden Road, Mount Eden, Auckland.'),
+('USR0011', 'ilham', '202cb962ac59075b964b07152d234b70', '0', 'Ilham Saputrajati', '089666445551', 'ilhamsaputrajati@gmail.com', 'Girisuko, Kec. Panggang, Kab. Gunung Kidul Prov. D.I. Yogyakarta');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merk`
+--
+
+CREATE TABLE `merk` (
+  `id_merk` varchar(10) NOT NULL,
+  `merk_mbl` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `merk`
+--
+
+INSERT INTO `merk` (`id_merk`, `merk_mbl`) VALUES
+('MRK0001', 'Toyota'),
+('MRK0002', 'Suzuki'),
+('MRK0003', 'Mitsubishi'),
+('MRK0004', 'Datsun'),
+('MRK0005', 'Isuzu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` varchar(11) NOT NULL,
+  `id_merk` varchar(10) NOT NULL,
+  `no_polisi` varchar(10) NOT NULL,
+  `model` varchar(20) NOT NULL,
+  `warna` varchar(10) NOT NULL,
+  `tahun` int(4) NOT NULL,
+  `hrg_beli` int(10) NOT NULL,
+  `hrg_jual` int(6) NOT NULL,
+  `tgl_beli` timestamp NULL DEFAULT NULL,
+  `tgl_jual` timestamp NULL DEFAULT NULL,
+  `gambar` varchar(50) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mobil`
+--
+
+INSERT INTO `mobil` (`id_mobil`, `id_merk`, `no_polisi`, `model`, `warna`, `tahun`, `hrg_beli`, `hrg_jual`, `tgl_beli`, `tgl_jual`, `gambar`, `deskripsi`) VALUES
+('MBL0002', 'MRK0005', 'AB6666ZZ', 'Koeneshig', 'Merah Meri', 1995, 10000005, 1000002, '2018-04-29 17:00:00', '0000-00-00 00:00:00', 'lmb.jpg', 'lorem lorem'),
+('MBL0012', 'MRK0004', 'AB6666ZZ', 'Go', 'DSDDD', 2, 12, 123, '2018-05-19 17:00:00', '0000-00-00 00:00:00', 'maxresdefault.jpg', 'lorem lorem'),
+('MBL0013', 'MRK0004', 'XXXXXX', 'JDSFKD', 'bdbkjf', 2010, 200000000, 123, '2018-05-22 17:00:00', '0000-00-00 00:00:00', 'saas.jpg', 'lorem lorem'),
+('MBL0014', 'MRK0002', 'AB2222CC', 'dfgd', 'dsdg', 2010, 50000000, 200000000, '2018-05-21 17:00:00', '0000-00-00 00:00:00', 'Toyota_Etios_L_1.jpg', 'lorem lorem'),
+('MBL0015', 'MRK0001', 'AB2222ZZ', 'AAAA', 'WWWW', 2201, 1500000, 2000000, '2018-05-30 17:00:00', '0000-00-00 00:00:00', 'harga xenia bekas.jpg', 'lorem lorem'),
+('MBL0016', 'MRK0003', 'slfnssnkf', 'ksadnkj', 'sjkdfs', 1221, 11211111, 2222222, '2018-05-23 17:00:00', '0000-00-00 00:00:00', 'mobil88-Autogard2.jpg', 'lorem lorem');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_bayar` varchar(11) NOT NULL,
+  `id_pemesanan` varchar(11) NOT NULL,
+  `id_member` varchar(7) NOT NULL,
+  `mtd_byr` enum('mandiri','bca') NOT NULL,
+  `total_byr` int(11) DEFAULT NULL,
+  `tgl_byr` timestamp NULL DEFAULT NULL,
+  `bukti_byr` varchar(50) NOT NULL,
+  `status` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan`
+--
+
+CREATE TABLE `pemesanan` (
+  `id_pemesanan` varchar(11) NOT NULL,
+  `id_member` varchar(11) NOT NULL,
+  `id_mobil` varchar(11) NOT NULL,
+  `tgl_pesan` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` enum('1','0') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `cars`
+-- Indexes for table `gallery`
 --
-ALTER TABLE `cars`
-  ADD PRIMARY KEY (`id_car`);
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`id_gallery`),
+  ADD KEY `id_mobil` (`id_mobil`);
 
 --
--- Indexes for table `confirm`
+-- Indexes for table `member`
 --
-ALTER TABLE `confirm`
-  ADD PRIMARY KEY (`id_confirm`);
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
 
 --
--- Indexes for table `transactions`
+-- Indexes for table `merk`
 --
-ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`id_transaction`);
+ALTER TABLE `merk`
+  ADD PRIMARY KEY (`id_merk`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `mobil`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`);
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`),
+  ADD KEY `id_merk` (`id_merk`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_bayar`),
+  ADD KEY `id_pemesanan` (`id_pemesanan`),
+  ADD KEY `id_member` (`id_member`);
+
+--
+-- Indexes for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD PRIMARY KEY (`id_pemesanan`),
+  ADD KEY `id_member` (`id_member`),
+  ADD KEY `id_mobil` (`id_mobil`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `cars`
+-- AUTO_INCREMENT for table `gallery`
 --
-ALTER TABLE `cars`
-  MODIFY `id_car` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+ALTER TABLE `gallery`
+  MODIFY `id_gallery` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `confirm`
+-- Constraints for dumped tables
 --
-ALTER TABLE `confirm`
-  MODIFY `id_confirm` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `transactions`
+-- Constraints for table `gallery`
 --
-ALTER TABLE `transactions`
-  MODIFY `id_transaction` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+ALTER TABLE `gallery`
+  ADD CONSTRAINT `gallery_ibfk_1` FOREIGN KEY (`id_mobil`) REFERENCES `mobil` (`id_mobil`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- AUTO_INCREMENT for table `users`
+-- Constraints for table `mobil`
 --
-ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `mobil`
+  ADD CONSTRAINT `mobil_ibfk_1` FOREIGN KEY (`id_merk`) REFERENCES `merk` (`id_merk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_2` FOREIGN KEY (`id_member`) REFERENCES `member` (`id_member`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pembayaran_ibfk_3` FOREIGN KEY (`id_pemesanan`) REFERENCES `pemesanan` (`id_pemesanan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`id_member`) REFERENCES `member` (`id_member`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pemesanan_ibfk_2` FOREIGN KEY (`id_mobil`) REFERENCES `mobil` (`id_mobil`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
